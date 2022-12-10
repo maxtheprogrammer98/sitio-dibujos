cd530ae30a7292518be6aa42d2ca43c1aa621fc5
Maximilian <72700320+maxtheprogrammer98@users.noreply.github.com>
2022-12-10 16:26:14 -0300

    Avances varios en galeria
    Modal ya funcional en cualquier resolucion, incluyendo la funcionalidad de imagen siguiente / anterior.

--- View/js/galeria.js
+++ View/js/galeria.js
@@ -1,7 +1,9 @@
 // ------------------ OBJETO ATRIBUTOS ------------------//
 let at = {
+    // elementos DOM:
     body: document.querySelector("body"),
     imagenes: document.querySelectorAll(".item_galeria img"),
+    // atributos abstractos:
     target: null,
     lightbox: null,
     modal: null,
@@ -10,7 +12,9 @@ let at = {
     iconoCerrar: null,
     botonAnt: null,
     botonSig: null,
-    contador: null
+    contador: 0,
+    // propiedades para configurar ubicacion:
+    topScrollPosition: null,
 }
 
 // ------------------ OBJETO METODOS ------------------//
@@ -30,18 +34,24 @@ let me = {
     getTarget:function(img){
         at.target = img.target;
         at.ID = img.target.id;
+        at.contador = parseInt(at.ID.replace("i",""));
+        at.topScrollPosition = window.pageYOffset
         me.crearLightbox();
     },
 
     crearLightbox:function(img){
+        // Se crea lightbox (fondo semi-transparente)
         at.body.appendChild(document.createElement("div")).setAttribute("id","lightbox");
         at.lightbox = document.querySelector("#lightbox");
+
+        // se especifica la ubicacion en la que se mostrara el elemento modal
+        at.topScrollPosition += 50;
+        at.lightbox.style.paddingTop = `${at.topScrollPosition}px`;
 
         me.crearModal(at.lightbox);
     },
 
     crearModal:function(elem){
-        console.log(at.target.id);
         //variables con elementos DOM a agregar:
         let imagen = at.target.outerHTML;
         let icono = "<img src='../Imagenes/cerrar-icon.png' alt='cerrar' id='icono_cerrar' >";
@@ -51,8 +61,13 @@ let me = {
                         <img src='../Imagenes/sig-icono.png' alt='boton' id='boton_sig'>
                        </div>`
         
+        // se agrega modal via appendchild:
         elem.appendChild(document.createElement("div")).setAttribute("id", "modal");
         at.modal = document.querySelector("#modal");
+
+        //se define como se mostrara el elemento modal dependiendo de la 
+        //resolucion del dispositivo:
+
 
         at.modal.appendChild(document.createElement("div")).setAttribute("id","wrapper_cerrar");
         at.wrapperCerrar = document.querySelector("#wrapper_cerrar");
@@ -61,31 +76,45 @@ let me = {
         at.iconoCerrar = document.querySelector("#icono_cerrar");
         
         at.modal.insertAdjacentHTML("beforeend",contenido);
+        at.contenedorImagen = document.querySelector("#contenedor_modal");
 
         at.modal.insertAdjacentHTML("beforeend", botones);
         at.botonAnt = document.querySelector("#boton_ant");
         at.botonSig = document.querySelector("#boton_sig");
 
-        at.botonAnt.addEventListener("click", me.imagenAnterior);
-        at.botonSig.addEventListener("click", me.imagenSiguiente);
+        // FUNCIONALIDA PARA CAMBIAR DE IMAGEN (siguiente / anterior)
+        at.botonAnt.addEventListener("click", function imagenAnterior(){
+            if (at.contador > 0){
+                at.contador --;
+                at.target = document.querySelector(`#i${at.contador}`);
+                contenido = `<div id="contenedor_modal"> ${at.target.outerHTML} </div>`;
+                at.modal.insertAdjacentHTML("beforeend",contenido);
+                console.log(at.contador);
+            }
+        });
+
+        at.botonSig.addEventListener("click", function imagenSiguiente(){
+            if (at.contador < at.imagenes.length){
+                at.contador ++;
+                at.target = document.querySelector(`#i${at.contador}`);
+                contenido = `<div id="contenedor_modal"> ${at.target.outerHTML} </div>`;
+                at.modal.insertAdjacentHTML("beforeend",contenido);
+                console.log(at.contador);
+            }
+    
+        });
 
         at.iconoCerrar.addEventListener("click", me.cerrarModal);
+        
 
         
     },
 
     cerrarModal:function(){
+        // cierra la ventana modal
         at.body.removeChild(at.lightbox);
-    },
-
-    imagenAnterior:function(){
-        // contador = parseInt(at.ID.replace("i",""));
-        // contador -= 1
-        // at.imagenes.forEach(element => {
-        //     if (element.id == `i${contador}`){
-        //         at.target = element;
-        //     }
-        // });
+        // se reseta el contador
+        at.contador = 0;
     }
 }
 
